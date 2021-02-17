17042ebe75f8bad506db06e7bcdc6813de62ae97
Christopher Lind <Christopher.lind.c@gmail.com>
2021-02-17 13:04:23 +0100

    Add project files.

---
+++ NewMovieDatabase/Search.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewMovieDatabase
+{
+    class Search
+    {
+        private string _searchString;
+        private bool _randomSearch;
+
+
+        public Search(string searchString, bool random = false)
+        {
+            _searchString = searchString;
+            _randomSearch = random;
+        }
+    }
+}
