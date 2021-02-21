62735e59ad353698e677b5c85f0fa98e166dad97
Christopher Lind <Christopher.lind.c@gmail.com>
2021-02-21 10:15:08 +0100

    added new tablecolumn collection todo

---
+++ NewMovieDatabase/TableClasses/ColumnType/TableColumnCollection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewMovieDatabase.TableClasses.ColumnType
+{
+    class TableColumnCollection : ColumnCollection
+    {
+
+        // TODO implement specific column collection for tables ie specify primary and secondary keys
+    }
+}
