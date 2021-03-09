f2d1e9485bef3322a62deab2926a2e298eaf7ff7
Christopher Lind <Christopher.lind.c@gmail.com>
2021-03-09 14:33:43 +0100

    TableAndColumnCreationTests Moved to NUnit and split into TableCreationTests and ColumnCreationTests

---
+++ TestProject/TableCreationTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using NewMovieDatabase.TableClasses;
+
+namespace TestProject
+{
+    public class TableCreationTests
+    {
+        string tableName = "testTable";
+        Table table;
+
+
+        // Tests related to the basic creation of tables
+        [SetUp]
+        public void SetupMethod()
+        {
+            table = new Table(tableName);
+        }
+
+        [Test]
+        public void TableToString()
+        {
+            Assert.AreEqual(tableName, table.ToString());
+            Assert.AreEqual(tableName, table.TableName);
+        }
+
+        [Test]
+        public void TableEmptyCollection()
+        {
+            Assert.AreEqual(0, table.ColumnCount);
+        }
+
+        [Test]
+        public void TableNoPrimaryKey()
+        {
+            Assert.IsFalse(table.HasPrimaryKey);
+        }
+    }
+}
